65461424bb9f12a2bc3bcb3cc25802cf44a60752
VincentInt <sanyabobiev@gmail.com>
2026-02-18 19:03:27 +0300

    adaptive space

--- src/Router/Layout/Background/Background.tsx
+++ src/Router/Layout/Background/Background.tsx
@@ -1,14 +1,24 @@
+import { useEffect, useState } from "react";
 import "./Background.css";
 import CursorLight from "./CursorLight/CursorLight";
 import RandomLights from "./RandomLights/RandomLights";
 
 const Background = () => {
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    setIsMobile(
+      /mobile|android|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(
+        navigator.userAgent,
+      ),
+    );
+  }, []);
   return (
     <div className="container_background">
       <div className="container_blur"></div>
       <div className="noises"></div>
       <RandomLights />
-      <CursorLight />
+      {isMobile ? "" : <CursorLight />}
     </div>
   );
 };
--- src/Router/Layout/Layout.tsx
+++ src/Router/Layout/Layout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-// import Background from "./Background/Background";
+import Background from "./Background/Background";
 import Header from "./Header/Header";
 // import Footer from "./Footer/Footer";
 
 const Layout = () => {
   return (
     <>
-      {/* <Background /> */}
+      <Background />
       <Header />
       <Outlet />
       {/* <Footer /> */}
